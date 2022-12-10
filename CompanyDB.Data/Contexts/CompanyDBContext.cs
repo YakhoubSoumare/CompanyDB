8ce637844a96768c6403792bc86686befaef05e6
Yakhoub Soumare <yakhoub.soumare@iths.com>
2022-12-10 12:00:40 +0100

    Add project files.

---
+++ CompanyDB.Data/Contexts/CompanyDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyDB.Data.Contexts;
+
+public class CompanyDBContext: DbContext
+{
+    public CompanyDBContext(DbContextOptions<CompanyDBContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
